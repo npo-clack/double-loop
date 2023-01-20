ef95a32fb4f615c7e31315d1d35ba3f25b8a2a50
KotaTakeda <takeda11.kl@outlook.jp>
2023-01-20 18:30:33 +0900

    refactoring

--- js/double-loop.js
+++ js/double-loop.js
@@ -5,93 +5,72 @@ const jIndexArea = document.getElementById("j-index-area");
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// create table
-const kukuTable = document.createElement("table");
-kukuTable.setAttribute("class", "table");
-
-// create head
-// loop 
-//   add th
-const thead = document.createElement("thead");
-const trHead = document.createElement("tr");
-
-// <th>#</th>
-const th = createTh("horizontal-#", "i \\ j");
-trHead.appendChild(th);
-
-// loop
-// <th>i</th>
-for (let i=1; i<=9; i++) {
-  const th = createTh("horizontal-" + String(i), String(i));
-  trHead.appendChild(th);
-}
-thead.appendChild(trHead);
-
-kukuTable.appendChild(thead);
-
-// create tbody
-// loop 
-//  create tr
-//  add th
-//    loop
-//      add td, id "i-j"
-const tbody = document.createElement("tbody");
-for (let i=1; i<=9; i++) {
-  const tr = document.createElement("tr");
-  const th = createTh("vertical-" + String(i), String(i));
-  tr.appendChild(th);
-  for (let j=1; j<=9; j++) {
-    const th = createTh(String(i) + "-" + String(j));
-    tr.appendChild(th);
-  }
-  tbody.appendChild(tr);
-}
-kukuTable.appendChild(tbody);
-
+// initialize table
+const kukuTable = createTable(numbers);
 tableArea.appendChild(kukuTable);
 
-
-
-
-// add component
+// set action
 startButton.onclick = () => {
-  if (i < 9 & j <9){
-    setInterval(calculate, 300);
-  }
-}
-
-let i = 1;
-let j = 1;
-calculate = () => {
-  const result = document.getElementById(String(i) + "-" + String(j));
-  console.log(String(i) + "-" + String(j));
-  result.innerText = i*j;
-
-  // colorize current number 
-  document.getElementById("vertical-" + String(i)).setAttribute("class", "bg-primary");
-  document.getElementById("horizontal-" + String(j)).setAttribute("class", "bg-primary");
-
-  // decolorize previous number
-  const prevJ = j == 1 ? 9 : j-1;
-  document.getElementById("horizontal-" + String(prevJ)).setAttribute("class", "bg-white");
-  if (j == 1 & i != 1) {
-    document.getElementById("vertical-" + String(i-1)).setAttribute("class", "bg-white");
+  // tableArea.removeChild(tableArea.firstChild);
+  // const kukuTable = createTable(numbers);
+  // tableArea.appendChild(kukuTable);
+  let j = 0;
+  let i = 0;
+  const calcId = setInterval(
+    () => {
+      calculate(i, j);
+      
+      if (j < numbers.length-1) {
+        j++;
+      } else if (i < numbers.length-1) {
+        i++;
+        j=0;
+      } else {
+        clearInterval(calcId);
+      }
+    }, 10);
   }
 
-  // 
-  iIndexArea.innerText = "i=" + String(i);
-  jIndexArea.innerText = "j=" + String(j);
+
+// functions
+// -----------
+// create table
+function createTable(numbers) {
+  const kukuTable = document.createElement("table");
+  kukuTable.setAttribute("class", "table");
   
-  if (j < 9) {
-    j++;
-  } else if (i < 9) {
-    i++;
-    j=1;
+  // create head
+  const thead = document.createElement("thead");
+  const trHead = document.createElement("tr");
+  // <th>#</th>
+  const th = createTh("horizontal-#", "a[i] \\ a[j]");
+  trHead.appendChild(th);
+  // loop
+  // <th>i</th>
+  for (let i=0; i<numbers.length; i++) {
+    const th = createTh("horizontal-" + String(numbers[i]), String(numbers[i]));
+    trHead.appendChild(th);
   }
+  thead.appendChild(trHead);
+  kukuTable.appendChild(thead);
+  
+  // create tbody
+  const tbody = document.createElement("tbody");
+  for (let i=0; i<numbers.length; i++) {
+    const tr = document.createElement("tr");
+    const th = createTh("vertical-" + String(numbers[i]), String(numbers[i]));
+    tr.appendChild(th);
+    for (let j=0; j<numbers.length; j++) {
+      const th = createTh(String(numbers[i]) + "-" + String(numbers[j]));
+      tr.appendChild(th);
+    }
+    tbody.appendChild(tr);
+  }
+  kukuTable.appendChild(tbody);
+
+  return kukuTable;
 }
 
-
-// utility functions
 function createTh(id, innerText="") {
   const th = document.createElement("th");
   th.setAttribute("scope", "row");
@@ -99,4 +78,26 @@ function createTh(id, innerText="") {
   th.innerText = innerText;
   th.setAttribute("id", id);
   return th
+}
+
+// calculate i*j and show
+function calculate(i, j) {
+  const result = document.getElementById(String(numbers[i]) + "-" + String(numbers[j]));
+  console.log(String(numbers[i]) + "-" + String(numbers[j]));
+  result.innerText = numbers[i]*numbers[j];
+  
+  // colorize current number 
+  document.getElementById("vertical-" + String(numbers[i])).setAttribute("class", "bg-secondary");
+  document.getElementById("horizontal-" + String(numbers[j])).setAttribute("class", "bg-secondary");
+  
+  // decolorize previous number
+  const prevJ = j == 0 ? numbers.length-1 : j-1;
+  document.getElementById("horizontal-" + String(numbers[prevJ])).setAttribute("class", "bg-white");
+  if (j == 0 & i != 0) {
+    document.getElementById("vertical-" + String(numbers[i-1])).setAttribute("class", "bg-white");
+  }
+  
+  // 
+  iIndexArea.innerText = "i=" + String(i);
+  jIndexArea.innerText = "j=" + String(j);
 }
